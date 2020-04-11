9d6379e5dfcb5e52e68deee87e69ed9286986c74
Fernand <fernando.vgarcia@hotmail.com>
2020-04-11 02:36:07 -0300

    Backend allowing communication with frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -10,6 +11,9 @@ mongoose.connect('mongodb+srv://fernando:cursoreact@cluster0-xo9rd.mongodb.net/w
     useUnifiedTopology: true
 });
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 //express understands body request as JSON
 app.use(express.json());
 app.use(routes);
