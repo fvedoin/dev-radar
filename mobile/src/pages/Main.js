d5aa5aff17d53a871bbd09d2d5edaaf06a706f1f
Fernand <fernando.vgarcia@hotmail.com>
2020-04-13 20:54:06 -0300

    Mobile receiving new user from websocket

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -6,7 +6,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
-import socket from '../services/socket';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
 
@@ -39,12 +39,23 @@ function Main({ navigation }) {
 
     }, []);
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]));
+    }, [devs]);
+
     function setupWebsocket(){
+        disconnect();
+
+        const { latitude, longitude } = currentRegion;
         
+        connect(
+            latitude,
+            longitude,
+            techs
+        );
     }
 
     async function loadDevs(){
-
         const { latitude, longitude } = currentRegion;
 
         const response = await api.get('/search', {
--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,7 +4,16 @@ const socket = socketio('http://10.0.2.2:3333', {
     autoConnect: false
 });
 
-function connect() {
+function subscribeToNewDevs(subscribeFunction) {
+    socket.on('new-dev', subscribeFunction);
+}
+
+function connect(latitude, longitude, techs) {   
+    socket.io.opts.query = {
+        latitude,
+        longitude,
+        techs
+    }
     socket.connect();
 }
 
@@ -16,5 +25,6 @@ function disconnect() {
 
 export {
     connect,
-    disconnect
+    disconnect,
+    subscribeToNewDevs
 }
